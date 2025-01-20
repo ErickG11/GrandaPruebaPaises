1d0005f34e9a200de1bf5b22bc2300a7318d2a1a
ErickG11 <grandaerick8@gmail.com>
2025-01-20 07:10:48 -0500

    Agregar archivos de proyecto.

---
+++ GrandaPruebaPaises/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GrandaPruebaPaises
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
